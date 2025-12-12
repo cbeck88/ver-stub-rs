af23540eb74555962348d7c4ea1e05a451b4bcb6
Chris Beck <beck.ct@gmail.com>
2025-12-12 15:00:17 -0700

    fix the fix

--- ver-stub-example-build/build.rs
+++ ver-stub-example-build/build.rs
@@ -3,5 +3,6 @@ fn main() {
         .with_all_git()
         .with_all_build_time()
         .patch_into_bin_dep("ver-stub-example", "ver-stub-example")
+        .with_filename(format!("ver-stub-example.bin{}", std::env::consts::EXE_SUFFIX))
         .write_to_target_profile_dir();
 }
