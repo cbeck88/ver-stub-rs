e366ec6bdc6361f1c795e080fc7ecb958ffcfb0d
Chris Beck <beck.ct@gmail.com>
2025-12-12 14:52:10 -0700

    fix example-build on windows

--- ver-stub-example-build/build.rs
+++ ver-stub-example-build/build.rs
@@ -3,6 +3,5 @@ fn main() {
         .with_all_git()
         .with_all_build_time()
         .patch_into_bin_dep("ver-stub-example", "ver-stub-example")
-        .with_filename("ver-stub-example.bin")
         .write_to_target_profile_dir();
 }
